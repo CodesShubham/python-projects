25647a8b6065ca940e108ec340c0f4fbb7a71234
69PercentFat <shubham11501@gmail.com>
2023-06-16 00:58:56 +0530

    making alexa speak

--- thelexa/main.py
+++ thelexa/main.py
@@ -1,11 +1,20 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say('I am your Bitch')
+engine.say('What can i do for you')
+engine.runAndWait()
 try:
     with sr.Microphone() as source:
         print('listening...')
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
-        print(command)
+        command = command.lower()
+        if 'alexa' in command:
+            print(command)
 except:
     pass
