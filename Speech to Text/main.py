a2a0bc69542ddd92b951a8193b3b49a333e77954
69PercentFat <shubham11501@gmail.com>
2023-06-16 11:32:43 +0530

    updated voices

--- Speech to Text/main.py
+++ Speech to Text/main.py
@@ -1,6 +1,13 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say('i am your Speech to Text tool')
+engine.say('Say something to convert')
+engine.runAndWait()
 try:
     with sr.Microphone() as source:
         print('listening...')
--- thelexa/main.py
+++ thelexa/main.py
@@ -1,12 +1,21 @@
 import speech_recognition as sr
+import pyttsx3
 
 listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+engine.say('i am your Speech to Text tool')
+engine.say('Say something to convert')
+engine.runAndWait()
 try:
     with sr.Microphone() as source:
         print('listening...')
         voice = listener.listen(source)
         command = listener.recognize_google(voice)
-        print(command)
+        command = command.lower()
+        if 'alexa' in command:
+            print(command)
 
 except:
     pass
