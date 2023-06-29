a0ecb6bc9ebb80d4955c8ddbb14c694c42eeff55
69PercentFat <shubham11501@gmail.com>
2023-06-30 00:33:19 +0530

    madlibs

---
+++ Madlibs/madlibs.py
@@ -0,0 +1,9 @@
+name = input("Name: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+adj = input("Adjective: ")
+
+madlib = f"I want to fuck {name} I like {verb1} with his balls \
+I also like {verb2} with his ass. I am a {adj} guy"
+
+print(madlib)
