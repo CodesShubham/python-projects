890b9fb436cd2966d34b2473250728eaf224f0e6
69PercentFat <shubham11501@gmail.com>
2023-06-30 00:55:22 +0530

    madlibs

--- Madlibs/madlibs.py
+++ Madlibs/madlibs.py
@@ -3,7 +3,7 @@ verb1 = input("Verb: ")
 verb2 = input("Verb: ")
 adj = input("Adjective: ")
 
-madlib = f"I want to fuck {name} I like {verb1} with his balls \
+madlib = f"I want to fuck{name}, I like {verb1} with his balls, \
 I also like {verb2} with his ass. I am a {adj} guy"
 
 print(madlib)
